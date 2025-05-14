af52dd726c1c85912479efca2fb56d710f157361
Faraotis <137304409+faraotis@users.noreply.github.com>
2025-05-14 15:06:12 -0300

    Implementei a opção de pesquisa, e otimizei o html

--- arquivos/scripts.js
+++ arquivos/scripts.js
@@ -1,6 +1,7 @@
 let divMenu = document.querySelector('#menu')
 let navMenu = document.querySelector('nav')
 var cont = 1
+const inputSearch = document.getElementById('search')
 
 function openMenu() {
     cont++
@@ -26,4 +27,42 @@ if (agoraHor < 12) {
 } else {
     // NOITE
     display.innerHTML = 'Volte sempre e tenha uma boa noite!'
+}
+
+
+//PESQUISAR
+
+inputSearch.addEventListener('input', (event) => {
+    const value = formatString(event.target.value)
+
+    const items = document.querySelectorAll('.items .item')
+    const noResults = document.getElementById('no_results')
+
+    let hasResults = false
+
+    items.forEach(item => {
+        const itemTitle = item.querySelector('.item-title').textContent
+
+        if(formatString(itemTitle).indexOf(value) !== -1) {
+            item.style.display = 'flex'
+
+            hasResults = true
+        } else {
+            item.style.display = 'none'
+        }
+    })
+
+    if (hasResults) {
+        noResults.style.display = 'none'
+    } else {
+        noResults.style.display = 'block'
+    }
+})
+
+function formatString(value) {
+    return value
+        .toLowerCase()
+        .trim()
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
 }
